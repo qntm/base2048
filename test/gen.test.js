bf0a6baebd735af8c20eef9b4063530c1561d4f8
qntm <qntm@users.noreply.github.com>
2025-12-22 21:28:30 +0000

    Update gen.test.js

--- test/gen.test.js
+++ test/gen.test.js
@@ -1,5 +1,5 @@
 import assert from 'node:assert/strict'
-import { describe, it } from 'node:test'
+import { describe, it, before } from 'node:test'
 
 import { genPromise } from '../scripts/gen.js'
 
